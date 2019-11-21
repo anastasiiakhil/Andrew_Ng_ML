01cba1fa8a715354030c96500e7536073e577340
anastasiiakhil <staceymakfin@gmail.com>
2019-11-21 17:48:12 +0200

    fifth task

--- ml_ex5/ex5_1.py
+++ ml_ex5/ex5_1.py
@@ -5,7 +5,17 @@ from scipy.optimize import minimize
 
 
 def data_visualization(X, y):
-    plt.scatter(X[:, 1], y, c='red', marker='+')
+    plt.scatter(X[:, 1], y, c='red', marker='x')
+    plt.title('Data')
+    plt.xlabel('Change in water level')
+    plt.ylabel('Water flowing out of the dam')
+    plt.show()
+
+
+def data_with_line(X, y, line):
+    plt.scatter(X[:, 1], y, c='red', marker='x')
+    plt.plot(X[:, 1], line)
+    plt.title('Linear Fit')
     plt.xlabel('Change in water level')
     plt.ylabel('Water flowing out of the dam')
     plt.show()
@@ -33,15 +43,49 @@ def train_linear_reg(X, y, reg_param):
     return new_theta
 
 
+def learning_curve(X, y, Xval, yval, reg_param):
+    m = X.shape[0]
+    error_train = np.zeros((m, 1))
+    error_val = np.zeros((m, 1))
+    for i in range(1, m+1):
+        theta = train_linear_reg(X[:i], y[:i], reg_param)
+        error_train[i-1] = linear_reg_cost(X[:i], y[:i], theta, 0)[0]
+        error_val[i-1] = linear_reg_cost(Xval, yval, theta, 0)[0]
+    return error_train, error_val
+
+
+def curve_visualization(X, error_train, error_val):
+    m = X.shape[0]
+    plt.plot(range(1, m+1), error_train, c='blue', label='Train')
+    plt.plot(range(1, m+1), error_val, c='green', label='Cross Validation')
+    plt.title('Learning curve for linear regression')
+    plt.xlabel('Number of training examples')
+    plt.ylabel('Error')
+    plt.legend()
+    plt.show()
+
+
+def poly_features(X, p):
+    X_poly = X
+    for i in range(1, p):
+        X_poly = np.vstack((X_poly, np.power(X, i+1)))
+    return X_poly
+
+
+def data_normalization(x):
+    mu = np.mean(x, axis=0)
+    sigma = np.std((x - mu), axis=0)
+    X_norm = (x - mu)/sigma
+    return X_norm, mu, sigma
+
+
 def main():
     data = loadmat('ex5data1.mat')
     X, y = data['X'], data['y']
     Xval, yval  = data['Xval'], data['yval']
     Xtest, ytest = data['Xtest'], data['ytest']
-
-    m = X.shape[0]
-    X = np.hstack((np.ones((m, 1)), X))
-
+    X = np.hstack((np.ones((X.shape[0], 1)), X))
+    Xval = np.hstack((np.ones((Xval.shape[0], 1)), Xval))
     # data_visualization(X, y)
 
     theta = np.ones((X.shape[1], 1))
@@ -50,6 +94,25 @@ def main():
 
     theta_min = train_linear_reg(X, y, 0)
     print('Theta using scipy.optimize.minimize:', theta_min)
+    # data_with_line(X, y, np.dot(X, theta_min))
+
+    error_train, error_val = learning_curve(X, y, Xval, yval, 0)
+    # curve_visualization(X, error_train, error_val)
+
+   # Feature Mapping for Polynomial Regression
+    p = 8
+    poly_X = poly_features(X[:, 1], p)
+    poly_X, mu, sigma = data_normalization(poly_X)
+    poly_X = np.hstack((np.ones((poly_X.shape[0], 1)), poly_X))
+
+    poly_test_X = poly_features(Xtest, p)
+    poly_test_X, mu, sigma = data_normalization(poly_test_X)
+    poly_test_X = np.hstack((np.ones((poly_test_X.shape[0], 1)), poly_test_X))
+
+    poly_val_X = poly_features(Xval, p)
+    poly_val_X = poly_val_X - mu
+    poly_val = poly_val_X / sigma
+    poly_val_X = np.hstack((np.ones((poly_val_X.shape[0], 1)), poly_val_X))
 
 
 main()
