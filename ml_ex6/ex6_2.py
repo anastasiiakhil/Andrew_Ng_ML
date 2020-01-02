a65952c9c7cb3e8e9bce243e6fa6701ee42ce63f
anastasiiakhil <staceymakfin@gmail.com>
2020-01-02 23:10:54 +0200

    sixth task

--- ml_ex6/ex6_2.py
+++ ml_ex6/ex6_2.py
@@ -1,5 +1,9 @@
 import re
 from nltk.stem import PorterStemmer
+import numpy as np
+from scipy.io import loadmat
+from sklearn.svm import SVC
+import pandas as pd
 
 
 def vocab_dict(list):
@@ -36,6 +40,29 @@ def process_email(email, vocab):
     return word_indices
 
 
+def email_features(word_indices, vocab):
+    n = len(vocab)
+    features = np.zeros((n, 1))
+    for i in word_indices:
+        features[i] = 1
+    return features
+
+
+def top_predictors(spam_svc, vocab):
+    weights = spam_svc.coef_[0]
+    n = len(weights)
+    weights = np.hstack((np.arange(1, n+1).reshape(n, 1),weights.reshape(n, 1)))
+    weights_df = pd.DataFrame(weights)
+    weights_df.sort_values(by=[1],ascending = False,inplace=True)
+
+    predictors = []
+    for i in weights_df[0][:15]:
+        for keys, values in vocab.items():
+            if str(int(i)) == values:
+                predictors.append(keys)
+    return predictors
+
+
 def main():
     file_contents = open('emailSample1.txt', 'r').read()
     vocab_list = open('vocab.txt', 'r').read()
@@ -43,6 +70,24 @@ def main():
     vocab_list = vocab_list.split('\n')[:-1]
     vocab = vocab_dict(vocab_list)
     word_indices = process_email(file_contents, vocab)
+    features = email_features(word_indices, vocab)
+
+    # print("Length of feature vector: ", len(features))
+    # print("Number of non-zero entries: ", np.sum(features))
+
+    spam_train = loadmat('spamTrain.mat')
+    X_train, y_train = spam_train['X'], spam_train['y']
+    spam_svc = SVC(C=0.1, kernel='linear')
+    spam_svc.fit(X_train, y_train.ravel())
+    print("Training Accuracy:", (spam_svc.score(X_train, y_train.ravel())) * 100, "%")
+
+    spam_test = loadmat('spamTest.mat')
+    X_test, y_test = spam_test['Xtest'], spam_test['ytest']
+    spam_svc.predict(X_test)
+    print("Test Accuracy:", (spam_svc.score(X_test, y_test.ravel())) * 100, "%")
+
+    main_predictors = top_predictors(spam_svc, vocab)
+    print('Top predictors of spam: ', main_predictors)
 
 
 main()
